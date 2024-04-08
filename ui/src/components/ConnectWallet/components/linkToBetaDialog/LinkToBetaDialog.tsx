e5bd21a2ee6b317bbb28eed2a37e2dd03591545e
kimi@zypher.game <kimi@zypher.game>
2024-04-08 09:40:34 +0800

    delete font

--- ui/src/components/ConnectWallet/components/linkToBetaDialog/LinkToBetaDialog.tsx
+++ ui/src/components/ConnectWallet/components/linkToBetaDialog/LinkToBetaDialog.tsx
@@ -30,7 +30,6 @@ const DialogButton = styled.div`
 
   color: #fff;
   text-align: center;
-  font-family: PixeloidSans;
   font-size: 16px;
   cursor: pointer;
   display: flex;
@@ -40,7 +39,6 @@ const DialogButton = styled.div`
 const Text = styled.div`
   color: #fff;
   text-align: center;
-  font-family: PixeloidSans;
   font-size: 14px;
   padding-top: 30px;
 `;
