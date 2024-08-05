8227caa67cc83c7a63da888937d431d4ff258228
HaiSli <admin@192.168.0.20>
2024-08-05 17:31:40 +0800

    Bump version to 0.1.123

--- ui/dist/rainbow/Ton.d.ts
+++ ui/dist/rainbow/Ton.d.ts
@@ -5,16 +5,4 @@ export declare const TonChainInfo: {
 };
 export declare const tonProvider: any;
 export declare const tonWeb: any;
-export declare const WebAppData: {
-    query_id: string;
-    user: {
-        id: number;
-        first_name: string;
-        last_name: string;
-        username: string;
-        language_code: string;
-        allows_write_to_pm: boolean;
-    };
-    auth_date: string;
-    hash: string;
-};
+export declare const WebAppData: any;
