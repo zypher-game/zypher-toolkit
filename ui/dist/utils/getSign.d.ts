4956cafdc00bfbe4d7dbcc65a7d0fdae6c001d6c
Admin <adfsda.game@yy.com>
2024-10-11 15:04:23 +0800

    Bump version to 0.2.11

--- ui/dist/utils/getSign.d.ts
+++ ui/dist/utils/getSign.d.ts
@@ -1,4 +1,4 @@
-export declare function getWeb3Sign(dataToSign: string, account: string, isArrayify: boolean | undefined, walletClient: any): Promise<boolean | string>;
+export declare function getWeb3Sign(dataToSign: string, account: string, isArrayify?: boolean, walletClient?: any): Promise<boolean | string>;
 export declare function getEIP712Sign({ domain, types, data, account, }: {
     domain: any;
     types: any;
