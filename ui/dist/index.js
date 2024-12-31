fc635f8f2176d26d4b4ba2a636052c5ec2c78180
Admin <adfsda.game@yy.com>
2024-12-31 11:17:46 +0800

    Bump version to 0.3.40

--- ui/dist/index.js
+++ ui/dist/index.js
@@ -5617,9 +5617,17 @@ var pointsBalanceState = atom7({
 
 // src/components/Staking/GP/constant/GPConstant.ts
 var GPAddress = {
+  ["59144" /* LineaMainnet */]: {
+    GP: "0x6ba3593101E32cEdBDE5AC9439e9187736B26A15",
+    Store: "0xc93BD0Ac4C2Ec3721e24ceAFcF8a7C2ae572F553"
+  },
   ["59141" /* LineaSepolia */]: {
     GP: "0x91D416d939baA3Aa822DD1B776fC5e9610b952C2",
     Store: "0xE877BACFd073A9E35C12F55C40ad89ad88Ed72b2"
+  },
+  ["9901" /* ZytronLineaMain */]: {
+    GP: "0x9aa0e7639e385437236686797d4210d60C9b9E1E",
+    Store: "0x5B0B09C52c5D7B22276253eE65779c8f6D81Fc76"
   },
   ["50098" /* ZytronLineaSepoliaTestnet */]: {
     GP: "0xF37D91f603F8E72648249b3D4D555cE26F8612C8",
--- ui/src/components/Staking/GP/constant/GPConstant.ts
+++ ui/src/components/Staking/GP/constant/GPConstant.ts
@@ -2,9 +2,17 @@ import { ChainId } from "../../../../constant/constant";
 import { Address } from "viem";
 
 export const GPAddress = {
+  [ChainId.LineaMainnet]: {
+    GP: "0x6ba3593101E32cEdBDE5AC9439e9187736B26A15",
+    Store: "0xc93BD0Ac4C2Ec3721e24ceAFcF8a7C2ae572F553",
+  },
   [ChainId.LineaSepolia]: {
     GP: "0x91D416d939baA3Aa822DD1B776fC5e9610b952C2",
     Store: "0xE877BACFd073A9E35C12F55C40ad89ad88Ed72b2",
+  },
+  [ChainId.ZytronLineaMain]: {
+    GP: "0x9aa0e7639e385437236686797d4210d60C9b9E1E",
+    Store: "0x5B0B09C52c5D7B22276253eE65779c8f6D81Fc76",
   },
   [ChainId.ZytronLineaSepoliaTestnet]: {
     GP: "0xF37D91f603F8E72648249b3D4D555cE26F8612C8",
